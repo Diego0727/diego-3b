ec002b8c1d5b3c04f5c7b5efe332933bd9f14502
Esteban-Martinez-44 <a3521110037@alumno.uttehuacan.edu.mx>
2023-07-18 23:17:00 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//Funcion declarativa
+function login() {
+     var example;
+     const exampleTwo;
+     let exampleThree;
+     console.log("Hola mundo...!")
+}
---
+++ segundo.js
@@ -0,0 +1,18 @@
+//Funcion declarativa
+function name0Funtion() {
+    var example;
+    const exampleTwo;
+    let exampleThree;
+}
+// expresion de funciones
+const name0Function = function() {
+   var example;
+   const exampleTwo;
+   let exampleThree;
+}
+// arrow function
+const name0Funtion = () => {
+   var example;
+   const exampleTwo;
+   let exampleThree;
+}
